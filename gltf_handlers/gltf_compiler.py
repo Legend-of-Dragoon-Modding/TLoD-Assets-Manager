3cceee20c4d45bec320ea1b598100e2bf75d6fe3
DragoonSouls <dragoonsouls@gmail.com>
2024-07-31 04:01:10 -0300

    ALPHA 0.1

--- gltf_handlers/gltf_compiler.py
+++ gltf_handlers/gltf_compiler.py
@@ -8,6 +8,7 @@ Copyright (C) 2024 DooMMetaL
 """
 
 import gc
+import struct
 
 class NewModel:
     def __init__(self, model_data=dict, animation_data=dict | None, file_name=str) -> None:
@@ -20,53 +21,61 @@ class NewModel:
     def model_arrager(self) -> None:
         gltf_descriptor_data: dict = {}
         gltf_to_binary_data: dict = {}
+        gltf_accessors: dict = {}
 
         tmd_model_data = self.model_data.get(f'Converted_Data')
         tmd_model_objects_number = len(self.model_data.get(f'Data_Table'))
 
-        this_incremental_index = 0
+        this_meshes_index = 0
         for tmd_object_number in range(0, tmd_model_objects_number):
             get_this_object = tmd_model_data.get(f'Object_Number_{tmd_object_number}')
             get_vertex_this_object = get_this_object.get(f'Vertex')
             get_normal_this_object = get_this_object.get(f'Normal')
             get_primitives_this_object = get_this_object.get(f'Primitives')
-            processes_primitives_to_gltf = self.primitive_tmd_to_gltf_binary(primitives_to_process=get_primitives_this_object)
-            uv_buffers = processes_primitives_to_gltf[0]
-            color_buffers = processes_primitives_to_gltf[1]
-            
-            this_position = this_incremental_index
-            this_normal = this_incremental_index + 1
-            this_texcoord = this_incremental_index + 2
-            this_color_0 = this_incremental_index + 3
-            this_color_1 = this_incremental_index + 4
-            this_indices = this_incremental_index + 5
+            object_converted = self.primitive_tmd_to_gltf_binary(primitives_to_process=get_primitives_this_object, 
+                                                                 vertex_array=get_vertex_this_object, normal_array=get_normal_this_object)
+            object_buffers = object_converted[0]
+            elements_count = object_converted[1]
+
+            this_position = this_meshes_index
+            this_normal = this_meshes_index + 1
+            this_texcoord = this_meshes_index + 2
+            this_color_0 = this_meshes_index + 3
+            this_indices = this_meshes_index + 4
             this_material = tmd_object_number
 
             this_attributes: dict = {'POSITION': this_position, 'NORMAL': this_normal,
-                                     'TEXCOORD_0': this_texcoord, 
-                                     'COLOR_0': this_color_0, 'COLOR_1': this_color_1}
+                                     'TEXCOORD_0': this_texcoord, 'COLOR_0': this_color_0}
+            this_gltf_primitive: dict = {'attributes': this_attributes, 'indices': this_indices, 'material': this_material}
+            
+            meshes: dict = {f'Object_Number_{tmd_object_number}': this_gltf_primitive}
+            buffers_to_compile_binary: dict = {f'Object_Number_{tmd_object_number}': object_buffers}
 
-            this_gltf_primitive = {f'PrimObj_{tmd_model_objects_number}': 
-                                   {f'{self.file_name}_ObjNum_{tmd_model_objects_number}': 
-                                    {'attributes': this_attributes, 'indices': this_indices, 'material': this_material}}}
+            this_object_accessor_position: dict = {'bufferView': this_position, 'componentType': 5126, 'count': elements_count * 3, 'type': 'VEC3'}
+            this_object_accessor_normal: dict = {'bufferView': this_normal, 'componentType': 5126, 'count': elements_count * 3, 'type': 'VEC3'}
+            this_object_accessor_textcoord: dict = {'bufferView': this_texcoord, 'componentType': 5126, 'count': elements_count * 3, 'type': 'VEC2'}
+            this_object_accessor_color_0: dict = {'bufferView': this_color_0, 'componentType': 5126, 'count': elements_count * 3, 'type': 'VEC3'}
+            this_object_accessor_vertex_indices: dict = {'bufferView': this_indices, 'componentType': 5123, 'count': elements_count, 'type': 'SCALAR'}
+            this_object_accessor_normal_indices: dict = {'bufferView': this_indices, 'componentType': 5123, 'count': elements_count, 'type': 'SCALAR'}
 
-            descriptor_json: dict = {f'Object_Number_{tmd_object_number}': this_gltf_primitive}
-            buffers: dict = {'indexVertices': get_vertex_this_object, 
-                             'IndicesNormals': get_normal_this_object, 
-                             'UV': uv_buffers, 'Color': color_buffers}
-            to_compile_binary: dict = {f'Object_Number_{tmd_object_number}': buffers}
-            gltf_descriptor_data.update(descriptor_json)
-            gltf_to_binary_data.update(to_compile_binary)
+            this_accessor: dict = {f'Object_Number_{tmd_object_number}': 
+                                   {'AccPos': this_object_accessor_position, 'AccNor': this_object_accessor_normal,
+                                    'AccTex': this_object_accessor_textcoord, 'AccCol': this_object_accessor_color_0,
+                                    'AccVerInd': this_object_accessor_vertex_indices, 'AccNorInd': this_object_accessor_normal_indices}}
+            
+            gltf_descriptor_data.update(meshes)
+            gltf_to_binary_data.update(buffers_to_compile_binary)
+            gltf_accessors.update(this_accessor)
 
-            this_incremental_index += 6
+            this_meshes_index += 5
         
         del self.model_data
         del self.animation_data
         gc.collect()
         
-        self.gltf_format = {'Descriptor': gltf_descriptor_data, 'Buffers': gltf_to_binary_data}
+        self.gltf_format = {'Descriptor': gltf_descriptor_data, 'Buffers': gltf_to_binary_data, 'Accessors': gltf_accessors}
     
-    def primitive_tmd_to_gltf_binary(self, primitives_to_process=dict) -> tuple[dict, dict]:
+    def primitive_tmd_to_gltf_binary(self, primitives_to_process=dict, vertex_array=dict, normal_array=dict) -> tuple[dict, int]:
         """
         Take TMD-Primitives Data and gather UV, Color\n
         to generate UV and Colors Buffers for glTF Binary Format.\n
@@ -109,26 +118,67 @@ class NewModel:
                     
                     new_primitive_number += 1
         # Creating UV and Colors Buffers, since in TMD Primitive Format all the buffers are stored in the primitive itself, except for Vertices and Normals
-        # VERY BIG TODO: NEED TO SUPER SPLIT MORE AND MORE THIS
-        uv_buffer_dict: dict = {}
-        color_buffer_dict: dict = {}
-        this_index = 0
-        
+        # All elements will share the count, since i'm filling all the data in there (more zeros, eventually i will do it as proper)
+        primitives_buffers_this_object: dict = {}
+        this_object_vertices_array: list = []
+        this_object_normals_array: list = []
+        this_object_uv_array: list = []
+        this_object_color_array: list = []
+        this_object_index_vertices_array: list = []
+        this_object_index_normals_array: list = []
+        total_primitives = 0
         for this_new_primitive in new_primitives:
             get_attributes = new_primitives.get(f'{this_new_primitive}')
-            adjust_attributes = self.generate_buffers(attributes=get_attributes, current_index=this_index)
-            this_uv_buffer = adjust_attributes[0]
-            this_color_buffer = adjust_attributes[1]
+            generated_buffers = self.generate_buffers(attributes=get_attributes, vertex_array=vertex_array, normal_array=normal_array)
+            vertices_get = generated_buffers[0]
+            normals_get = generated_buffers[0]
+            uv_get = generated_buffers[0]
+            colors_get = generated_buffers[0]
+            index_vertices_get = generated_buffers[0]
+            index_normals_get = generated_buffers[0]
+            this_object_vertices_array.append(vertices_get)
+            this_object_normals_array.append(normals_get)
+            this_object_uv_array.append(uv_get)
+            this_object_color_array.append(colors_get)
+            this_object_index_vertices_array.append(index_vertices_get)
+            this_object_index_normals_array.append(index_normals_get)
+            total_primitives += 1
+        
+        # TODO: Meassure Length and storing bytes in each position with the corresponding length
+        join_object_vertices = b''.join(this_object_vertices_array)
+        join_object_normals = b''.join(this_object_normals_array)
+        join_object_uv = b''.join(this_object_uv_array)
+        join_object_color = b''.join(this_object_color_array)
+        join_object_vertices_index = b''.join(this_object_index_vertices_array)
+        join_object_normals_index = b''.join(this_object_index_normals_array)
 
-            single_line_uv_buffer = {f'{this_new_primitive}': this_uv_buffer}
-            uv_buffer_dict.update(single_line_uv_buffer)
+        length_object_vertices_array = len(join_object_vertices)
+        length_object_normals_array = len(join_object_normals)
+        length_object_uv_array = len(join_object_uv)
+        length_object_color_array = len(join_object_color)
+        length_object_vertices_index_array = len(join_object_vertices_index)
+        length_object_normals_index_array = len(join_object_normals_index)
 
-            single_line_color_buffer = {f'{this_new_primitive}': this_color_buffer}
-            color_buffer_dict.update(single_line_color_buffer)
+        single_array_block = [join_object_vertices, join_object_normals, join_object_uv, join_object_color, join_object_vertices_index, join_object_normals_index]
+        join_single_array_block = b''.join(single_array_block)
+        
+        length_object_array_block = len(join_single_array_block)
+        each_length_arrays = [length_object_vertices_array, length_object_normals_array, length_object_uv_array, length_object_color_array, 
+                              length_object_vertices_index_array, length_object_normals_index_array]
+        
+        offset_vertices = 0
+        offset_normal = length_object_vertices_array
+        offset_uv = length_object_vertices_array + length_object_normals_array
+        offset_color = length_object_vertices_array + length_object_normals_array + length_object_uv_array
+        offset_vertices_index = length_object_vertices_array + length_object_normals_array + length_object_uv_array + length_object_color_array
+        offset_normals_index = length_object_vertices_array + length_object_normals_array + length_object_uv_array + length_object_color_array + length_object_vertices_index_array
+        
+        sequenced_lengths = [offset_vertices, offset_normal, offset_uv, offset_color, offset_vertices_index, offset_normals_index]
 
-            this_index += 1
+        primitives_buffers_this_object = {'Buffers': join_single_array_block, 'BufferFullLength': length_object_array_block,
+                                          'LengthEachArray': each_length_arrays, 'SequencedLenghts': sequenced_lengths}
 
-        return uv_buffer_dict, color_buffer_dict
+        return primitives_buffers_this_object, total_primitives
     
     def split_quad_primitive(self, primitive_properties=dict) -> tuple[dict, dict]:
         """
@@ -183,15 +233,27 @@ class NewModel:
 
         return new_primitive_1, new_primitive_2
 
-    def generate_buffers(self, attributes=dict) -> tuple[list, list]:
+    def generate_buffers(self, attributes=dict, vertex_array=dict, normal_array=dict) -> list:
         """
         Generate Buffers:\n
         This Method will take the data from TMD-Primitives and will generate the buffers\n
         for glTF Binary Format, this include IndexVertices, IndexNormals, \n
-        UV, Color, Vertex and Normal data into two list to be send to the Binary Compiler.
+        UV, Color, Vertex and Normal, converting it into Bytes. This is the Data Format:\n
+        POSITION=VEC3 ; Float32Array\n
+        NORMAL=VEC3 ; Float32Array\n
+        UV=VEC2 ; Float32Array\n
+        COLOR_0=VEC3 ; Float32Array\n
+        Vertices_indices=SCALAR ; UNSIGNED_INT\n
+        Normals_indices=SCALAR ; UNSIGNED_INT\n
         """
+        new_vertex_array: list = []
+        new_normal_array: list = []
+        vertex_index: list = []
+        normal_index: list = []
         uv_attribute: list = []
         color_attribute: list = []
+
+        this_primitive_single_buffer: list = {}
 
         get_vertex_0 = attributes.get('vertex0')
         get_vertex_1 = attributes.get('vertex1')
@@ -232,9 +294,106 @@ class NewModel:
             r_0 = r_1 = r_2 = r_0
             g_0 = g_1 = g_2 = g_0
             b_0 = b_1 = b_2 = b_0
+        
+        # Vertex 0 Properties and Array into Binary
+        get_vertex_vectors_0 = vertex_array.get(f'{f'Vertex_Number_{get_vertex_0}'}')
+        get_x_0 = struct.pack('<f', get_vertex_vectors_0.get(f'VecX'))
+        get_y_0 = struct.pack('<f', get_vertex_vectors_0.get(f'VecY'))
+        get_z_0 = struct.pack('<f', get_vertex_vectors_0.get(f'VecZ'))
+        new_vertex_array.append(get_x_0)
+        new_vertex_array.append(get_y_0)
+        new_vertex_array.append(get_z_0)
+        get_vertex_0_bin = int.to_bytes(0, length=2, byteorder='little', signed=False)
 
-        uv_attribute = [u_0, v_0, u_1, v_1, u_2, v_2]
-        color_attribute = [r_0, g_0, b_0, r_1, g_1, b_1, r_2, g_2, b_2]
+        # Vertex 1 Properties and Array into Binary
+        get_vertex_vectors_1 = vertex_array.get(f'Vertex_Number_{get_vertex_1}')
+        get_x_1 = struct.pack('<f', get_vertex_vectors_1.get(f'VecX'))
+        get_y_1 = struct.pack('<f', get_vertex_vectors_1.get(f'VecY'))
+        get_z_1 = struct.pack('<f', get_vertex_vectors_1.get(f'VecZ'))
+        new_vertex_array.append(get_x_1)
+        new_vertex_array.append(get_y_1)
+        new_vertex_array.append(get_z_1)
+        get_vertex_1_bin = int.to_bytes(1, length=2, byteorder='little', signed=False)
 
-        return uv_attribute, color_attribute
+        # Vertex 2 Properties and Array into Binary
+        get_vertex_vectors_2 = vertex_array.get(f'Vertex_Number_{get_vertex_2}')
+        get_x_2 = struct.pack('<f', get_vertex_vectors_2.get(f'VecX'))
+        get_y_2 = struct.pack('<f', get_vertex_vectors_2.get(f'VecY'))
+        get_z_2 = struct.pack('<f', get_vertex_vectors_2.get(f'VecZ'))
+        new_vertex_array.append(get_x_2)
+        new_vertex_array.append(get_y_2)
+        new_vertex_array.append(get_z_2)
+        get_vertex_2_bin = int.to_bytes(2, length=2, byteorder='little', signed=False)
+
+        # Normal 0 Properties and Array into Binary
+        get_normal_vectors_0 = normal_array.get(f'{f'Normal_Number_{get_normal_0}'}')
+        get_n_x_0 = struct.pack('<f', get_normal_vectors_0.get(f'VecX'))
+        get_n_y_0 = struct.pack('<f', get_normal_vectors_0.get(f'VecY'))
+        get_n_z_0 = struct.pack('<f', get_normal_vectors_0.get(f'VecZ'))
+        new_normal_array.append(get_n_x_0)
+        new_normal_array.append(get_n_y_0)
+        new_normal_array.append(get_n_z_0)
+        get_normal_0_bin = int.to_bytes(0, length=2, byteorder='little', signed=False)
+
+        # Normal 1 Properties and Array into Binary
+        get_normal_vectors_1 = normal_array.get(f'{f'Normal_Number_{get_normal_1}'}')
+        get_n_x_1 = struct.pack('<f', get_normal_vectors_1.get(f'VecX'))
+        get_n_y_1 = struct.pack('<f', get_normal_vectors_1.get(f'VecY'))
+        get_n_z_1 = struct.pack('<f', get_normal_vectors_1.get(f'VecZ'))
+        new_normal_array.append(get_n_x_1)
+        new_normal_array.append(get_n_y_1)
+        new_normal_array.append(get_n_z_1)
+        get_normal_1_bin = int.to_bytes(1, length=2, byteorder='little', signed=False)
+
+        # Normal 2 Properties and Array into Binary
+        get_normal_vectors_2 = normal_array.get(f'{f'Normal_Number_{get_normal_2}'}')
+        get_n_x_2 = struct.pack('<f', get_normal_vectors_2.get(f'VecX'))
+        get_n_y_2 = struct.pack('<f', get_normal_vectors_2.get(f'VecY'))
+        get_n_z_2 = struct.pack('<f', get_normal_vectors_2.get(f'VecZ'))
+        new_normal_array.append(get_n_x_2)
+        new_normal_array.append(get_n_y_2)
+        new_normal_array.append(get_n_z_2)
+        get_normal_2_bin = int.to_bytes(2, length=2, byteorder='little', signed=False)
+
+        # UV Data into Binary
+        u_0_bin = struct.pack('<f', u_0)
+        v_0_bin = struct.pack('<f', v_0)
+        u_1_bin = struct.pack('<f', u_1)
+        v_1_bin = struct.pack('<f', v_1)
+        u_2_bin = struct.pack('<f', u_2)
+        v_2_bin = struct.pack('<f', v_2)
+
+        # Vertex 0 RGB Calcs and into Binary
+        calc_r_0 = struct.pack('<f', round(float(r_0 / 256), 12))
+        calc_g_0 = struct.pack('<f', round(float(g_0 / 256), 12))
+        calc_b_0 = struct.pack('<f', round(float(b_0 / 256), 12))
+
+        # Vertex 1 RGB Calcs and into Binary
+        calc_r_1 = struct.pack('<f', round(float(r_1 / 256), 12))
+        calc_g_1 = struct.pack('<f', round(float(g_1 / 256), 12))
+        calc_b_1 = struct.pack('<f', round(float(b_1 / 256), 12))
+
+        # Vertex 1 RGB Calcs and into Binary
+        calc_r_2 = struct.pack('<f', round(float(r_2 / 256), 12))
+        calc_g_2 = struct.pack('<f', round(float(g_2 / 256), 12))
+        calc_b_2 = struct.pack('<f', round(float(b_2 / 256), 12))
+
+        uv_attribute = [u_0_bin, v_0_bin, u_1_bin, v_1_bin, u_2_bin, v_2_bin]
+        color_attribute = [calc_r_0, calc_g_0, calc_b_0, calc_r_1, calc_g_1, calc_b_1, calc_r_2, calc_g_2, calc_b_2]
+
+        # Adding Pad Bytes to the end of the Vertex Index and Normal Index
+        padding_value = b'\x00\x00'
+        vertex_index = [get_vertex_0_bin, get_vertex_1_bin, get_vertex_2_bin, padding_value]
+        normal_index = [get_normal_0_bin, get_normal_1_bin, get_normal_2_bin, padding_value]
+
+        new_vertex_array_join = b''.join(new_vertex_array)
+        new_normal_array_join = b''.join(new_normal_array)
+        uv_attribute_join = b''.join(uv_attribute)
+        color_attribute_join = b''.join(color_attribute)
+        vertex_index_join = b''.join(vertex_index)
+        normal_index_join = b''.join(normal_index)
+
+        this_primitive_single_buffer = [new_vertex_array_join, new_normal_array_join, uv_attribute_join, color_attribute_join, vertex_index_join, normal_index_join]
+
+        return this_primitive_single_buffer
 
--- gltf_handlers/gltf_converter.py
+++ gltf_handlers/gltf_converter.py
@@ -17,7 +17,7 @@ DEVELOPER NOTE: since i'm new at this format expect a lot of non expected
 behaviors and code stupidity //SORRY
 
 """
-
+import struct
 from pygltflib import *
 
 class gltfFile:
@@ -35,27 +35,150 @@ class gltfFile:
     def convert_gltf(self) -> None:
         """This actually do the Conversion"""
         descriptor_gltf = self.gltf_to_convert.get(f'Descriptor')
-        binary_gltf = self.gltf_to_convert.get(f'Buffers')
+        to_binary_buffers_gltf = self.gltf_to_convert.get(f'Buffers')
+        accessors_data_gltf = self.gltf_to_convert.get(f'Accessors')
+        
         # Setup an empty glTF File
         gltf_file = GLTF2()
+
+        # Setup Asset
+        gltf_file.asset.generator = f'TLoD Assets Converter ALPHA 0.1'
+        gltf_file.asset.version = f'2.0'
+        gltf_file.extensionsUsed = [f'KHR_materials_specular', f'KHR_materials_ior']
+        gltf_file.scene = 0
+        
         # Setup an Scene
         gltf_scene = Scene()
         gltf_scene.name = 'Scene'
-        # Setup number of nodes
         get_total_number_nodes = len(descriptor_gltf)
         for this_node in range(0, get_total_number_nodes):
             gltf_scene.nodes.append(this_node)
         gltf_file.scenes.append(gltf_scene)
+        
+        # Setup Nodes
+        for this_node_number in range(0, get_total_number_nodes):
+            gltf_nodes = Node()
+            gltf_nodes.mesh = this_node_number
+            gltf_nodes.name =  f'{self.gltf_file_name}_ObjNum_{this_node_number}'
+            gltf_file.nodes.append(gltf_nodes)
 
-        # Setup Meshes (for each node, since TLoD use object by object Animation)
-        for this_gltf_object in descriptor_gltf:
-            get_this_object = descriptor_gltf.get(f'{this_gltf_object}')
-            change_name_gltf_object = this_gltf_object.replace('Object_Number_', f'{self.gltf_file_name}_ObjNum_')
-            # Setup the Mesh
+        # Setup Materials - HERE WE ARE IMPLEMENTING VERY BASIC MATERIALS (In future i hope implementing specific materials for each objects)
+        for this_object_material in range(0, get_total_number_nodes):
+            material_gltf = Material()
+            material_gltf.doubleSided = True
+            material_gltf.extensions = {'KHR_materials_specular': {'specularFactor': 0}, 
+                                        'KHR_materials_ior': {'ior': 1.4499999284744263}}
+            material_gltf.name = f'{self.gltf_file_name}_ObjNum_{this_object_material}'
+            pbr_settings = PbrMetallicRoughness()
+            pbr_settings.baseColorFactor = [0.800000011920929, 0.800000011920929, 0.800000011920929, 1]
+            pbr_settings.metallicFactor = 0.0
+            pbr_settings.roughnessFactor = 0.5
+            material_gltf.pbrMetallicRoughness = pbr_settings
+            material_gltf.alphaCutoff = None
+            gltf_file.materials.append(material_gltf)
+
+        # Setup Meshes (for each node, since TLoD use object by object Animation) -> And Primitives
+        # Maybe i could add a single mesh and each primitives block is represented by an object? who knows
+        for this_gltf_mesh in descriptor_gltf:
+            get_this_object = descriptor_gltf.get(f'{this_gltf_mesh}')
+            change_name_gltf_object = this_gltf_mesh.replace('Object_Number_', f'{self.gltf_file_name}_ObjNum_')
             gltf_mesh = Mesh()
             gltf_mesh.name = change_name_gltf_object
-            for this_primitive in get_this_object:
-                get_this_primitive = get_this_object.get(f'{this_primitive}')
-                gltf_primitive = Primitive()
-                gltf_primitive.attributes.POSITION = get_this_primitive.get('Vertices')
-                gltf_primitive.attributes.NORMAL = get_this_primitive.get('Normals')
+            gltf_primitive = Primitive()
+            get_this_primitive_attributes = get_this_object.get('attributes')
+            gltf_primitive.attributes.POSITION = get_this_primitive_attributes.get('POSITION')
+            gltf_primitive.attributes.NORMAL = get_this_primitive_attributes.get('NORMAL')
+            gltf_primitive.attributes.TEXCOORD_0 = get_this_primitive_attributes.get('TEXCOORD_0')
+            gltf_primitive.attributes.COLOR_0 = get_this_primitive_attributes.get('COLOR_0')
+            gltf_primitive.indices = get_this_object.get('indices')
+            gltf_primitive.material = get_this_object.get('material')
+            gltf_mesh.primitives.append(gltf_primitive)
+            gltf_file.meshes.append(gltf_mesh)
+        
+        # Setup Accessors
+        # POSITION=VEC3 ; NORMAL=VEC3 ; NORMAL=VEC2 ; COLOR_0=VEC3 ; indices=SCALAR
+        # TODO: I THINK I DO ANYTHING BUT A GOOD CALCULATION FOR accessor_gltf.count = get_accessor_data.get('count')
+        # ERROR: ACCESSOR_TOO_LONG -> Accessor (offset: %1, length: %2) does not fit referenced bufferView [%3] length %4.
+        for this_object in accessors_data_gltf:
+            current_accessors_arrays = accessors_data_gltf.get(f'{this_object}')
+            for this_accessor in current_accessors_arrays:
+                accessor_gltf = Accessor()
+                get_accessor_data = current_accessors_arrays.get(f'{this_accessor}')
+                accessor_gltf.bufferView = get_accessor_data.get('bufferView')
+                accessor_gltf.componentType = get_accessor_data.get('componentType')
+                accessor_gltf.count = get_accessor_data.get('count')
+                accessor_gltf.type = get_accessor_data.get('type')
+                gltf_file.accessors.append(accessor_gltf)
+        
+        # Setup BufferViews and Accessors
+        buffers_binary_final: list = []
+        byte_offset_previous = 0
+        for this_object in to_binary_buffers_gltf:
+            get_buffer_object = to_binary_buffers_gltf.get(f'{this_object}')
+            get_binary_data_buffer = get_buffer_object.get(f'Buffers')
+            get_buffer_full_length = get_buffer_object.get(f'BufferFullLength')
+            get_length_each_array = get_buffer_object.get(f'LengthEachArray') # This is each of the buffer length of bytes
+            get_length_sum_arrays = get_buffer_object.get(f'SequencedLenghts') # This is each of the summatory of previous lengths but only for this single object
+
+            vertex_buffer_gltf = BufferView()
+            vertex_buffer_gltf.buffer = 0
+            vertex_buffer_gltf.byteLength = get_length_each_array[0]
+            vertex_buffer_gltf.byteOffset = byte_offset_previous + get_length_sum_arrays[0]
+            vertex_buffer_gltf.target = 34962
+
+            normal_buffer_gltf = BufferView()
+            normal_buffer_gltf.buffer = 0
+            normal_buffer_gltf.byteLength = get_length_each_array[1]
+            normal_buffer_gltf.byteOffset = byte_offset_previous + get_length_sum_arrays[1]
+            normal_buffer_gltf.target = 34962
+
+            uv_buffer_gltf = BufferView()
+            uv_buffer_gltf.buffer = 0
+            uv_buffer_gltf.byteLength = get_length_each_array[2]
+            uv_buffer_gltf.byteOffset = byte_offset_previous + get_length_sum_arrays[2]
+            uv_buffer_gltf.target = 34962
+
+            color_buffer_gltf = BufferView()
+            color_buffer_gltf.buffer = 0
+            color_buffer_gltf.byteLength = get_length_each_array[3]
+            color_buffer_gltf.byteOffset = byte_offset_previous + get_length_sum_arrays[3]
+            color_buffer_gltf.target = 34962
+
+            ivertex_buffer_gltf = BufferView()
+            ivertex_buffer_gltf.buffer = 0
+            ivertex_buffer_gltf.byteLength = get_length_each_array[4]
+            ivertex_buffer_gltf.byteOffset = byte_offset_previous + get_length_sum_arrays[4]
+            ivertex_buffer_gltf.target = 34963
+
+            inormal_buffer_gltf = BufferView()
+            inormal_buffer_gltf.buffer = 0
+            inormal_buffer_gltf.byteLength = get_length_each_array[5]
+            inormal_buffer_gltf.byteOffset = byte_offset_previous + get_length_sum_arrays[5]
+            inormal_buffer_gltf.target = 34963
+
+            gltf_file.bufferViews.append(vertex_buffer_gltf)
+            gltf_file.bufferViews.append(normal_buffer_gltf)
+            gltf_file.bufferViews.append(uv_buffer_gltf)
+            gltf_file.bufferViews.append(color_buffer_gltf)
+            gltf_file.bufferViews.append(ivertex_buffer_gltf)
+            gltf_file.bufferViews.append(inormal_buffer_gltf)
+
+            buffers_binary_final.append(get_binary_data_buffer)
+
+            byte_offset_previous += get_buffer_full_length
+
+        # Creating Binary Buffers in GLB Binary Format
+        buffer_binary_joined_final = b''.join(buffers_binary_final)
+        length_buffer_binary = len(buffer_binary_joined_final)
+        
+        binary_file_name = f'{self.gltf_file_name}_buffers.bin'
+        buffer_glb_gltf = Buffer()
+        buffer_glb_gltf.byteLength = length_buffer_binary
+        buffer_glb_gltf.uri = binary_file_name
+        gltf_file.buffers.append(buffer_glb_gltf)
+
+        gltf_file.save_json(f'{self.gltf_deploy_path}.gltf')
+
+        with open(f'{self.gltf_deploy_path}_buffers.bin', 'wb') as gltf_binary_buffer:
+            gltf_binary_buffer.write(buffer_binary_joined_final)
+            gltf_binary_buffer.close()
